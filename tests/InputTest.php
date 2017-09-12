9b726927be083ada3f5b3e0f0705d36923c9600d
Dima Loburets <loburets@a2design.biz>
2017-09-12 17:40:52 +0600

    Added input tests with empty bodies

---
+++ tests/InputTest.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'FormBuilderTestBase.php';
+
+class InputTest extends FormBuilderTestBase
+{
+    public function testOpening()
+    {
+        //
+    }
+
+    public function testErrors()
+    {
+        //
+    }
+
+    public function testAttrsParameter()
+    {
+        //
+    }
+
+    public function testCommonInputAttributes()
+    {
+        //
+    }
+
+    public function testClasses()
+    {
+        //
+    }
+
+    public function testOnlyInputParameter()
+    {
+        //
+    }
+
+    public function testInputGroupParameter()
+    {
+        //
+    }
+
+    public function testLabel()
+    {
+        //
+    }
+}
