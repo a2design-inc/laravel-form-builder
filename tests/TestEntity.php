5735ea167fe0f8c31cb25267ced3dab1d4928edd
Dima Loburets <loburets@a2design.biz>
2017-09-14 14:40:01 +0600

    Added new fields for test model stub

--- tests/TestEntity.php
+++ tests/TestEntity.php
@@ -11,4 +11,8 @@ class TestEntity extends Model
 {
     public $fieldWithValue = 'some-value';
     public $fieldWithOld = 'some-value';
+    public $fieldTrue = true;
+    public $fieldFalse = false;
+    public $fieldTrueString = '1';
+    public $fieldFalseString = '0';
 }
